be04b3483807894c16077eb97c496461e4b35b49
damilolaonitiju <83452297+damilolaonitiju@users.noreply.github.com>
2023-08-24 07:32:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerButton = document.querySelector(".c-hamburger");
-  const navMenu = document.querySelector(".c-nav_menu");
-  const closeButton = document.querySelector(".c-close");
+const hamburgerButton = document.querySelector(".c-hamburger");
+const navMenu = document.querySelector(".c-nav_menu");
+const closeButton = document.querySelector(".c-close");
 
-  hamburgerButton.addEventListener("click", function () {
-    navMenu.classList.toggle("show");
-    closeButton.classList.remove("uc-desktop-hide");
-  });
+hamburgerButton.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  closeButton.classList.remove("uc-desktop-hide");
+});
 
-  closeButton.addEventListener("click", function () {
-    navMenu.classList.remove("show");
-    closeButton.classList.add("uc-desktop-hide");
-  });
+closeButton.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+  closeButton.classList.add("uc-desktop-hide");
 });
